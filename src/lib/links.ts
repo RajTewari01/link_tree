be5cb3c93b434fcddae536f97a573ff1867bdddc
BISWADEEP TEWARI <tewari765@gmail.com>
2026-03-17 21:15:52 +0530

    Update links.ts
    Feat: updated the buymeacoffee link

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -19,7 +19,7 @@ export const USER_LINKS: LinkData[] = [
     id: "consulting",
     title: "Contribute",
     subtitle: "support my work",
-    url: "https://www.buymeacoffee.com/RajTewari01",
+    url: "https://buymeacoffee.com/RajTewari01",
     iconUrl: `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2"><path d="M18 8h1a4 4 0 0 1 0 8h-1"></path><path d="M2 8h16v9a4 4 0 0 1-4 4H6a4 4 0 0 1-4-4V8z"></path><line x1="6" y1="1" x2="6" y2="4"></line><line x1="10" y1="1" x2="10" y2="4"></line><line x1="14" y1="1" x2="14" y2="4"></line></svg>`
   },
   {
