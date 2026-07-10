539e7c8c9f748ed8ad9f30582e488b39a8c3ad95
BISWADEEP TEWARI <tewari765@gmail.com>
2026-07-10 21:39:26 +0530

    Update links.ts

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -36,10 +36,17 @@ export const USER_LINKS: LinkData[] = [
     iconUrl: `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>`
   },
   {
-    id: "youtube",
-    title: "YouTube",
-    subtitle: "Subscribe for more content",
+    id: "educational youtube",
+    title: "Educational YouTube",
+    subtitle: "Subscribe for more prod-based knowledge.",
     url: "https://www.youtube.com/@codingbybiswadeep",
+    iconUrl: `<svg width="24" height="24" viewBox="0 0 24 24" fill="#000000"><path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"></path></svg>`
+  },
+{
+    id: "gaming youtube",
+    title: "Gaming YouTube",
+    subtitle: "Subscribe for more prod-based knowledge.",
+    url: "https://www.youtube.com/@gamewithbiswadeep",
     iconUrl: `<svg width="24" height="24" viewBox="0 0 24 24" fill="#000000"><path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"></path></svg>`
   },
     {
